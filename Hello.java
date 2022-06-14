078d7ec99b882ac914c40c9335c75d5893177a23
Admin <2780516865@qq.com>
2022-06-14 10:42:48 +0800

    first commit

---
+++ Hello.java
@@ -0,0 +1,15 @@
+public class Hello {
+
+//一个类只能有一个main方法
+
+public static void main(String arg[]) {
+
+System.out.print("helloworld!");//输出不换行
+
+System.out.println("heng");//输出换行
+
+System.out.print("jadh");
+
+}
+
+}
